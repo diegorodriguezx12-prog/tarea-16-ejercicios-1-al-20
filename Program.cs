98e6c452554acb7011c2a778670f909efcf6d514
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-05 09:22:30 -0600

    ejercicio 11 terminado

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
-﻿int numero = 1;
+﻿int intentos = 0;
+int suma = 0;
+int numerosingresados;
 do
 {
-    Console.WriteLine(numero);
-    numero++;
-} while (numero <= 5);
+    Console.WriteLine("Ingrese el número:");
+    numerosingresados = int.Parse(Console.ReadLine());
+    suma = suma + numerosingresados;
+    intentos++;
+} while (intentos < 5);
+Console.WriteLine("La suma total es de: " + suma);
