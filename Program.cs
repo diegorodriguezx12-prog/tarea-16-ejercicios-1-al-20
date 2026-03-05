7d1e44de0cee4abcf1e8778e2a4a6d930cd2a6f1
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-05 09:55:36 -0600

    ejercicio 14 terminado

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
-﻿int numero = 1;
+﻿int numero;
+int mayor = 0;
+bool primerNumero = true;
 do
 {
-    Console.WriteLine(numero);
-    numero++;
-} while (numero <= 5);
+    Console.WriteLine("ingrese un número (Ingrese 0 para salir)");
+    numero = int.Parse(Console.ReadLine());
+    if (numero != 0)
+    {
+       if (primerNumero || numero>mayor)
+        {
+            mayor = numero;
+            primerNumero = false;
+        }
+    }
+} while (numero != 0);
+if (!primerNumero)
+{
+    Console.WriteLine("El número mayor ingresado fué: "+mayor);
+}
