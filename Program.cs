c73492f10b17374a23d9773592c3d738fbc21dd7
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-05 17:38:31 -0600

    ejercicio 15 terminado

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
-﻿int numero = 1;
+﻿string pass;
 do
 {
-    Console.WriteLine(numero);
-    numero++;
-} while (numero <= 5);
+    Console.WriteLine("Ingrese la contraseña");
+    pass = Console.ReadLine();
+    if (pass == "1234")
+    {
+        Console.WriteLine("Contraseña correcta");
+    }
+    else
+    {
+        Console.WriteLine("Contraseña incorrecta");
+    }
+} while (pass != "1234");
