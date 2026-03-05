37ec9d455d71dc331a492453198fc2caf910c4e2
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-05 17:51:33 -0600

    ejercicio 17 terminado

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
-﻿int numero = 1;
+﻿int numero;
+int contador = 0;
 do
 {
-    Console.WriteLine(numero);
-    numero++;
-} while (numero <= 5);
+    Console.WriteLine("Ingrese un número (0 para salir)");
+    numero = int.Parse(Console.ReadLine());
+    if (numero != 0 && numero % 2 == 0)
+    {
+        contador++;
+    }
+} while (numero != 0);
+Console.WriteLine("El total de números pares es de: " + contador);
