f5b4b5e8e24449d7857d22a483a5d8c73f96a3dd
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-05 09:01:16 -0600

    Ejercicio 10 terminado

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿int numero = 1;
+int contador = 0;
 do
 {
     Console.WriteLine(numero);
+    contador++;
     numero++;
-} while (numero <= 5);
+} while (numero <= 50);
+Console.WriteLine("La cantidad total de números es de: " + contador);
