f2d89787ce3a925efb2baaf16303e035ca18477e
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-05 18:20:24 -0600

    ejercicio 19 terminado

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
-﻿int numero = 1;
+﻿int tabla;
+int i = 1;
+Console.WriteLine("¿Qué tabla de multiplicar desea ver?");
+tabla = int.Parse(Console.ReadLine());
 do
 {
-    Console.WriteLine(numero);
-    numero++;
-} while (numero <= 5);
+    int resultado = tabla * i;
+    Console.WriteLine(tabla + " * " + i + " = " + resultado);
+    i++;
+} while (i <= 10);
+Console.WriteLine("Fin de la tabla del " + tabla);
