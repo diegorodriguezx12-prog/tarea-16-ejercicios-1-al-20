1ea994143a75a7c869bc6e2328ed994dadac425a
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-05 18:30:18 -0600

    ejercicio 20 terminado

--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
-﻿int numero = 1;
+﻿int numero;
+int sumaTotal = 0;
+int contador = 0;
+double promedio = 0;
 do
 {
-    Console.WriteLine(numero);
-    numero++;
-} while (numero <= 5);
+    Console.WriteLine("Ingrese un número (0 para finalizar): ");
+    numero = int.Parse(Console.ReadLine());
+
+    if (numero != 0)
+    {
+        sumaTotal = sumaTotal + numero;
+        contador++;
+    }
+
+} while (numero != 0);
+if (contador > 0)
+{
+    promedio = (double)sumaTotal / contador;
+
+    Console.WriteLine("Suma total: " + sumaTotal);
+    Console.WriteLine("Cantidad de números: " + contador);
+    Console.WriteLine("Promedio: " + promedio);
+}
+else
+{
+    Console.WriteLine("No se ingresaron números para calcular.");
+}
